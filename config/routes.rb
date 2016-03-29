fc95e611c520cd3ec21087665fb99f92cdb56208
brenodamata <bjrdamata@gmail.com>
2016-03-29 17:48:52 -0400

    Add friendship feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   resources :users do
     resources :exercises
   end
-  
+
   resource :dashboard, only: [:index] do
     collection do
       post :search, to: 'dashboard#search'
     end
   end
-  
+
+  resources :friendships
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
